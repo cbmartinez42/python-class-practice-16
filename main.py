e1b2f06cac3613aa292d90f60c679e550d98c410
Chris Martinez <chmartinez@lumeris.com>
2022-02-11 09:28:46 -0600

    adding turtle start module

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+from turtle import Turtle, Screen
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+timmy = Turtle()
+print(timmy)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+my_screen = Screen()
+print(my_screen.canvheight)
+timmy.shape('turtle')
+my_screen.exitonclick()
