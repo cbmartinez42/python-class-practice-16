048f2b18318cf541eb7b0171145925df67b5f111
Chris Martinez <chmartinez@lumeris.com>
2022-02-11 10:34:21 -0600

    adding table

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# print(timmy)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# timmy.shape('turtle')
+# timmy.color('coral')
+# timmy.forward(100)
+# timmy.color('blue')
+# timmy.left(200)
+# timmy.forward(300)
+#
+# my_screen.exitonclick()
 
-timmy = Turtle()
-print(timmy)
+from prettytable import PrettyTable
 
-my_screen = Screen()
-print(my_screen.canvheight)
-timmy.shape('turtle')
-my_screen.exitonclick()
+table = PrettyTable()
+table.add_column("Pokemon Name", ['Pikachu', 'Squirtle', 'Charmander'])
+table.add_column("Type", ['Electric', 'Water', 'Fire'])
+table.add_column("Pokemon Name", ['Pikachu', 'Squirtle', 'Charmander'])
+table.align = 'l'
+
+
+
+print(table)
