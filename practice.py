469ed45582431c190b451f05d0f24567cf584eb6
Chris Martinez <chmartinez@lumeris.com>
2022-02-11 11:13:51 -0600

    more class work

--- practice.py
+++ practice.py
@@ -2,17 +2,24 @@ class User:
     def __init__(self, user_id, name):
         self.id = user_id
         self.name = name
+        self.followers = 0
+        self.following = 0
         print('new user being created')
     
+    def is_star(self):
+        self.followers = 1000
     
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+
 user_1 = User('001', 'chris')
-# user_1.id = '001'
-# user_1.name = "chris"
-
 print(user_1.name)
-
 user_2 = User('002', "joe")
-# user_2.id = '002'
-# user_2.name = "joe"
+user_2.is_star()
+user_1.follow(user_2)
+print(
+    f"{user_1.name} has followed {user_2.name}. {user_2.name} now has {user_2.followers} and {user_1.name} is following {user_1.following} people")
 
-print(user_2.name)
+print(user_2.followers)
